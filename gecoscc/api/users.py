ebfd3e4e137814b257a325107c1b3c27565bd8b7
Antonio Pérez-Aranda Alcaide <ant30tx@gmail.com>
2013-12-04 08:18:12 +0100

    Require user logged to access the users resource

--- gecoscc/api/users.py
+++ gecoscc/api/users.py
@@ -1,12 +1,14 @@
-from gecoscc.models import User, Users
-from gecoscc.api import ResourcePaginated
-
 from cornice.resource import resource
+
+from gecoscc.api import ResourcePaginated
+from gecoscc.models import User, Users
+from gecoscc.permissions import api_login_required
 
 
 @resource(collection_path='/api/users/',
           path='/api/users/{oid}/',
-          description='Users resource')
+          description='Users resource',
+          validators=(api_login_required,))
 class UserResource(ResourcePaginated):
 
     schema_collection = Users
