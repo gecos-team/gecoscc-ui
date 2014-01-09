b8ff4f8efe5fd6bd0e63e0d2b2f82da301bb6306
Antonio Pérez-Aranda Alcaide <ant30tx@gmail.com>
2014-01-09 10:53:05 +0100

    Add the release name to package

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requires = [
 ]
 
 setup(name='gecoscc',
-      version='0.0',
+      version='0.2.1',
       description='gecoscc',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
