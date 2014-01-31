1defb65a4b211c6bfa5f7ce824160624e3b31f87
Antonio Pérez-Aranda Alcaide <ant30tx@gmail.com>
2014-01-31 12:22:53 +0100

    Define the storage json squema

--- gecoscc/policies/remote_storage.py
+++ gecoscc/policies/remote_storage.py
@@ -7,5 +7,28 @@ class RemoteStoragePolicy(BasePolicy):
     name = 'remote-storage'
     screen_name = _('Remote Storage')
 
+    schema = {
+        'type': 'object',
+        'properties': {
+            'name': {'type': 'string'},
+            'node': {'type': 'string'},
+            'storage_id': {'type': 'string'},
+            'storage_node': {
+                'type': 'object',
+                'properties':  {
+                    'memberof': {'type': 'array'},
+                    'server': {'type': 'string'},
+                    'port': {'type': 'integer'},
+                    'protocol': {'type': 'string'},
+                    'localpath': {'type': 'string'},
+                    'mount': {'type': 'string'},
+                    'extraops': {'type': 'string'},
+                },
+                'required': ['server', 'protocol', 'localpath', 'mount']
+            }
+        },
+        'required': ['name', 'node', 'storage_id']
+    }
+
     def translate_to_recipes(self):
         return {}
