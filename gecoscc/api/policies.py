3da1aff0654f63f5c5c4d27dc0fd8e7932ffc82a
Antonio Pérez-Aranda Alcaide <ant30tx@gmail.com>
2014-01-31 14:45:30 +0100

    Adding the policies resource file

---
+++ gecoscc/api/policies.py
@@ -0,0 +1,64 @@
+
+from cornice.resource import resource
+from pyramid.httpexceptions import HTTPNotFound
+
+from gecoscc.permissions import api_login_required
+
+from gecoscc.models import Policy, Policies
+
+from gecoscc.policies import PoliciesManager, PolicyDoesNotExist
+
+from . import ResourcePaginatedReadOnly
+
+
+@resource(collection_path='/api/policies/',
+          path='/api/policies/{name}/',
+          description='Policies resource',
+          validators=(api_login_required,))
+class PoliciesResource(ResourcePaginatedReadOnly):
+
+    schema_collection = Policies
+    schema_detail = Policy
+
+    objtype = 'policy'
+
+    def collection_get(self):
+        page = int(self.request.GET.get('page', 0))
+        pagesize = int(self.request.GET.get('pagesize', self.default_pagesize))
+
+        policies_manager = PoliciesManager()
+
+        extraargs = {}
+        if pagesize > 0:
+            extraargs.update({
+                'skip': page * pagesize,
+                'limit': pagesize,
+            })
+
+        objects = policies_manager.get_policies()
+
+        if pagesize > 0:
+            pages = int(len(objects) / pagesize)
+        else:
+            pagesize = 1
+        objects = objects[page * pagesize:]
+        objects = objects[:pagesize]
+        parsed_objects = self.parse_collection(list(objects))
+        return {
+            'pagesize': pagesize,
+            'pages': pages,
+            'page': page,
+            self.collection_name: parsed_objects,
+        }
+
+    def get(self):
+        name = self.request.matchdict['name']
+
+        policies_manager = PoliciesManager()
+
+        try:
+            policy = policies_manager.get_policy(name)
+        except PolicyDoesNotExist:
+            raise HTTPNotFound()
+
+        return self.parse_item(policy)
