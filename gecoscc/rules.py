4a2fbd0eab97875e6e8f17ad19c261df461ac80a
Pablo Martín <goinnn@gmail.com>
2014-04-24 12:40:23 +0200

    Adding rules to local file res

--- gecoscc/rules.py
+++ gecoscc/rules.py
@@ -16,12 +16,23 @@ for attr in rules_package_res_attrs:
     RULES_PACKAGE_RES['%s.%s' % (rules_package_res_path, attr)] = attr
 
 
+rules_local_file_res_path = 'gecos_ws_mgmt.misc_mgmt.local_file_res'
+rules_local_file_res_attrs = ('delete_files', 'copy_files')
+
+RULES_LOCAL_FILE_RES = {}
+
+for attr in rules_local_file_res_attrs:
+    RULES_LOCAL_FILE_RES['%s.%s' % (rules_local_file_res_path, attr)] = attr
+
+
 RULES_NODE = {'computer': {'save': {'gecos_ws_mgmt.network_mgmt.network_res.ip_address': 'ip'},
                            'policies': {'network_res': RULES_NETWORK_RES,
-                                        'package_res': RULES_PACKAGE_RES}},
+                                        'package_res': RULES_PACKAGE_RES,
+                                        'local_file_res': RULES_LOCAL_FILE_RES}},
               'ou': {'save': {},
                      'policies': {'network_res': RULES_NETWORK_RES,
-                                  'package_res': RULES_PACKAGE_RES}},
+                                  'package_res': RULES_PACKAGE_RES,
+                                  'local_file_res': RULES_LOCAL_FILE_RES}},
               'group': {'save': {},
                         'policies': {}},
               'user': {'save': {},
