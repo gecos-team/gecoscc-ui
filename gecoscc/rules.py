1a5ae7bbc41c20b98eb6feebe4899fd436091519
Pablo Martín <goinnn@gmail.com>
2014-04-24 13:14:12 +0200

    Adding scripts_launch_res rules

--- gecoscc/rules.py
+++ gecoscc/rules.py
@@ -1,3 +1,5 @@
+# Network rules
+
 rules_network_res_path = 'gecos_ws_mgmt.network_mgmt.network_res'
 rules_network_res_attrs = ('use_dhcp', 'netmask', 'ip_address', 'gateway', 'network_type', 'dns_servers')
 
@@ -6,6 +8,9 @@ RULES_NETWORK_RES = {}
 for attr in rules_network_res_attrs:
     RULES_NETWORK_RES['%s.%s' % (rules_network_res_path, attr)] = attr
 
+# end network rules
+
+# Package rules
 
 rules_package_res_path = 'gecos_ws_mgmt.software_mgmt.package_res'
 rules_package_res_attrs = ('package_list', 'pkgs_to_remove')
@@ -15,6 +20,9 @@ RULES_PACKAGE_RES = {}
 for attr in rules_package_res_attrs:
     RULES_PACKAGE_RES['%s.%s' % (rules_package_res_path, attr)] = attr
 
+# End package rules
+
+# Local file rules
 
 rules_local_file_res_path = 'gecos_ws_mgmt.misc_mgmt.local_file_res'
 rules_local_file_res_attrs = ('delete_files', 'copy_files')
@@ -23,12 +31,25 @@ RULES_LOCAL_FILE_RES = {}
 
 for attr in rules_local_file_res_attrs:
     RULES_LOCAL_FILE_RES['%s.%s' % (rules_local_file_res_path, attr)] = attr
+# End local file rules
+
+# Scripts launch rules
+
+rules_scripts_launch_res_path = 'gecos_ws_mgmt.misc_mgmt.scripts_launch_res'
+rules_scripts_launch_res_attrs = ('on_startup', 'on_shutdown')
+
+RULES_SCRIPTS_LAUNCH_RES = {}
+
+for attr in rules_scripts_launch_res_attrs:
+    RULES_SCRIPTS_LAUNCH_RES['%s.%s' % (rules_scripts_launch_res_path, attr)] = attr
+# End Scripts launch rules
 
 
 RULES_NODE = {'computer': {'save': {'gecos_ws_mgmt.network_mgmt.network_res.ip_address': 'ip'},
                            'policies': {'network_res': RULES_NETWORK_RES,
                                         'package_res': RULES_PACKAGE_RES,
-                                        'local_file_res': RULES_LOCAL_FILE_RES}},
+                                        'local_file_res': RULES_LOCAL_FILE_RES,
+                                        'scripts_launch_res': RULES_SCRIPTS_LAUNCH_RES}},
               'ou': {'save': {},
                      'policies': {'network_res': RULES_NETWORK_RES,
                                   'package_res': RULES_PACKAGE_RES,
