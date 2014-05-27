f103b958231f4132b958747ff84d1ddec9e61d58
Pablo Martín <goinnn@gmail.com>
2014-05-27 13:47:27 +0200

    New API. Returns the name and the chef_id of the computers

---
+++ gecoscc/api/public_computers.py
@@ -0,0 +1,25 @@
+from cornice.resource import resource
+
+from gecoscc.api import BaseAPI
+from gecoscc.models import Computer, Computers
+from gecoscc.permissions import http_basic_login_required
+
+
+@resource(path='/computers/list/',
+          description='Computers public API',
+          validators=(http_basic_login_required,))
+class ComputerPublicResource(BaseAPI):
+
+    schema_collection = Computers
+    schema_detail = Computer
+    objtype = 'computer'
+
+    mongo_filter = {
+        'type': objtype,
+    }
+    collection_name = 'nodes'
+
+    def get(self):
+        computers = [{'node_chef_id': comp['node_chef_id'],
+                      'name': comp['name']} for comp in self.collection.find({'type': self.objtype})]
+        return computers
