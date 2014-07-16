c6317ab2701953f2984e357778bb8507a9cb8186
Pablo Martín <goinnn@gmail.com>
2014-07-16 10:23:06 +0200

    Ready to new release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ requires = [
 ]
 
 setup(name='gecoscc',
-      version='0.6.1',
+      version='0.6.2',
       description='gecoscc',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
