9629016c16b232de95ba562b3d54810189271a36
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2014-08-29 12:46:56 +0200

    Add logging for exceptions

--- gecoscc/api/ad_import.py
+++ gecoscc/api/ad_import.py
@@ -544,6 +544,7 @@ class ADImport(BaseAPI):
                 'ok': True if successCounter == totalCounter else False
             }
         except Exception as e:
+            logging.exception(e)
             return {
                 'status': u'{0}'.format(e),
                 'ok': False
