94a1cf8ce5a37df1d56dbb0e0944a9774a9843e7
Pablo Martín <goinnn@gmail.com>
2014-10-02 12:18:46 +0200

    Ready to final release (0.7.X)

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ requires = [
 ]
 
 setup(name='gecoscc',
-      version='0.6.6',
+      version='0.7.0',
       description='gecoscc',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
