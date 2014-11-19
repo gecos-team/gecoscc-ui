085d0070fc53949bd03197ed441d6c77e3a2b6f3
Alberto Beiztegui <albertobeiz@gmail.com>
2014-11-19 13:50:18 +0100

    Ready to new release

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requires = [
 ]
 
 setup(name='gecoscc',
-      version='2.1.0',
+      version='2.1.1',
       description='gecoscc',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
