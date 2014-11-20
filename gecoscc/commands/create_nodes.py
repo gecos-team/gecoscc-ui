835c9c5180cd00ca2d35c4ade43d99867083b768
Pablo Martín <goinnn@gmail.com>
2014-11-20 16:49:43 +0100

    First version to create nodes command

--- gecoscc/commands/create_nodes.py
+++ gecoscc/commands/create_nodes.py
@@ -1,16 +1,23 @@
+import requests
+
 from optparse import make_option
 
 from bson import ObjectId
 
+from chef import Node as ChefNode
+from chef.node import NodeAttributes
+
 from gecoscc.management import BaseCommand
+from gecoscc.utils import get_chef_api
 
 
 class Command(BaseCommand):
     description = """
         Create nodes
     """
-    usage = ("usage: %prog create_nodes --administrator user --key file.pem --number 100000 "
-             "--organisational-unit-id 1234567890abcdef12345678 --copy-computer-id 1234567890abcdef12345678")
+    usage = ("usage: %prog create_nodes --number 100000 "
+             "--organisational-unit-id 1234567890abcdef12345678 --copy-computer-id 1234567890abcdef12345678 "
+             "--gecoscc-url http://localhost --gecoscc-username admin --gecoscc-password admin")
 
     option_list = [
         make_option(
@@ -34,23 +41,31 @@ class Command(BaseCommand):
             help='Computer id'
         ),
         make_option(
-            '-a', '--administrator',
-            dest='chef_username',
+            '-s', '--gecoscc-username',
+            dest='gcc_username',
             action='store',
-            help='An existing chef administrator username'
+            help='An existing gecoscc administrator username'
         ),
         make_option(
-            '-k', '--key',
-            dest='chef_pem',
+            '-p', '--gecoscc-password',
+            dest='gcc_password',
             action='store',
-            help='The pem file that contains the chef administrator private key'
+            help='The password of the gecoscc administrator'
+        ),
+        make_option(
+            '-u', '--gecoscc-url',
+            dest='gcc_url',
+            action='store',
+            help='The url where gecoscc is running'
+        ),
+        make_option(
+            '-r', '--prefix',
+            dest='prefix',
+            action='store',
+            help='Prefix',
+            default='scheherezade-'
         ),
     ]
-
-    required_options = (
-        'chef_username',
-        'chef_pem',
-    )
 
     def command(self):
         db = self.pyramid.db
@@ -62,6 +77,43 @@ class Command(BaseCommand):
         if not comp:
             print 'Error computer does not exists'
             return
+        node_id = comp.get('node_chef_id', None)
+        if not comp:
+            print 'Error this computer has not node_chef_id'
+            return
+        admin = db.adminusers.find_one({'username': self.options.gcc_username})
+        if not admin:
+            print 'Error this admin does not exists'
+            return
+        elif not admin.get('is_superuser', None):
+            print 'You need a super admin'
+            return
         number_nodes = int(self.options.number)
+        api = get_chef_api(self.settings,
+                           admin)
+        node = ChefNode(node_id, api)
         for i in range(number_nodes):
-            print i
+            new_node_name = '%s-%s' % (self.options.prefix, i)
+            new_node = ChefNode(new_node_name, api)
+            for attr in node.to_dict().keys():
+                if hasattr(node, attr) and attr != 'name':
+                    if attr == 'automatic':
+                        automatic_dict = node.automatic.to_dict()
+                        automatic_dict['ohai_gecos']['pclabel'] = new_node_name
+                        automatic = NodeAttributes(automatic_dict)
+                        setattr(new_node, attr, automatic)
+                    else:
+                        setattr(new_node, attr, getattr(node, attr))
+            new_node.save()
+            print 'Created %s at chef' % new_node_name
+            res = requests.post('%s/register/computer/' % self.options.gcc_url,
+                                {'ou_id': self.options.ou_id, 'node_id': new_node_name},
+                                auth=(self.options.gcc_username, self.options.gcc_password))
+            if res.ok and res.json()['ok']:
+                print 'Created %s at gcc' % new_node_name
+            elif res.ok and not res.json()['ok']:
+                print 'Error %s at gcc' % new_node_name
+                print '\t %s' % res.json()['message']
+            else:
+                print 'Unknow error %s at gcc' % new_node_name
+                
