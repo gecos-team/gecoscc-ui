ffd69817f5daa4f068c587fb86ffead15f263cb9
root <root@gccserver.(none)>
2017-06-02 09:50:37 +0200

    New merge algorithm.

--- gecoscc/utils.py
+++ gecoscc/utils.py
@@ -962,3 +962,28 @@ def update_computers_of_user(db, user, api):
                 user['computers'].append(node['_id'])
 
     return user
+
+def nested_lookup(key, document):
+    """Lookup a key in a nested document, return a list of values"""
+    return list(_nested_lookup(key, document))
+
+def _nested_lookup(key, document):
+    """Lookup a key in a nested document, yield a value"""
+    from six import iteritems
+
+    if isinstance(document, list):
+        for d in document:
+            for result in _nested_lookup(key, d):
+                yield result
+
+    if isinstance(document, dict):
+        for k, v in iteritems(document):
+            if k == key:
+                yield v
+            elif isinstance(v, dict):
+                for result in _nested_lookup(key, v):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    for result in _nested_lookup(key, d):
+                        yield result
