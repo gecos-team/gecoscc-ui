a1ad3dad2cba3751223063524699200673002617
Joroca <josem.rodriguez.caro@gmail.com>
2018-05-28 13:39:56 +0200

    Knife ec backup plugin with webui-key option

--- gecoscc/scripts/chef_restore.sh
+++ gecoscc/scripts/chef_restore.sh
@@ -20,5 +20,7 @@ KNIFE_BIN=/opt/opscode/bin/knife
 #   -s, --server-url URL             Chef Server URL
 #   -u, --user USER                  API Client Username
 #   -k, --key KEY                    API Client Key
+#   --webui-key                      Used to set the path to the WebUI Key. If your chef server is in a different machine,
+#                                    please, copy /etc/opscode/webui_priv.pem to this server and point this path to the file
 
-$KNIFE_BIN ec restore $1 -y -s $2 -u $3 -k $4
+$KNIFE_BIN ec restore $1 -y -s $2 -u $3 -k $4 --webui-key /etc/opscode/webui_priv.pem
