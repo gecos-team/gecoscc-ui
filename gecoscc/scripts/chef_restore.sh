f15534131497913e7b695147417e35b1bb46fffd
Joroca <josem.rodriguez.caro@gmail.com>
2018-05-29 07:45:09 +0200

    Removing -u,-k options from knife call

--- gecoscc/scripts/chef_restore.sh
+++ gecoscc/scripts/chef_restore.sh
@@ -13,14 +13,12 @@
 KNIFE_BIN=/opt/opscode/bin/knife
 
 # Checking number of arguments
-[[ $# -ne  4 ]] && echo "Illegal number of parameters" && exit 1
+[[ $# -ne 2 ]] && echo "Illegal number of parameters" && exit 1
 
 # knife-ec-backup plugin execution
 # Params:
 #   -s, --server-url URL             Chef Server URL
-#   -u, --user USER                  API Client Username
-#   -k, --key KEY                    API Client Key
 #   --webui-key                      Used to set the path to the WebUI Key. If your chef server is in a different machine,
 #                                    please, copy /etc/opscode/webui_priv.pem to this server and point this path to the file
 
-$KNIFE_BIN ec restore $1 -y -s $2 -u $3 -k $4 --webui-key /etc/opscode/webui_priv.pem
+$KNIFE_BIN ec restore $1 -y -s $2 --webui-key /etc/opscode/webui_priv.pem
