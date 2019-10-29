ab313a297ea39b890353586d3d687a876995aa44
System25 <amp_21004@yahoo.es>
2019-10-29 13:43:00 +0100

    Forbid using the same name as the domain name

--- gecoscc/api/__init__.py
+++ gecoscc/api/__init__.py
@@ -31,7 +31,8 @@ from gecoscc.tasks import (object_created, object_changed, object_deleted,
 from gecoscc.utils import (get_computer_of_user, get_filter_nodes_parents_ou,
                            oids_filter, check_unique_node_name_by_type_at_domain,
                            visibility_object_related, visibility_group,
-                           RESOURCES_EMITTERS_TYPES, get_object_related_list)
+                           RESOURCES_EMITTERS_TYPES, get_object_related_list,
+                           is_domain, get_domain)
 
 import gettext
 import logging
@@ -485,6 +486,15 @@ class TreeResourcePaginated(ResourcePaginated):
         if not unique:
             self.request.errors.add('body', 'name',
                                     "Name must be unique in domain.")
+        if unique and not is_domain(obj):
+            # Check that the node name is not the same as the domain name
+            domain = get_domain(obj, self.request.db.nodes)
+            if domain['name'] == obj['name']:
+                self.request.errors.add('body', 'name',
+                                    "Name not must be the domain name.")                
+                return False
+            
+        
         return unique
 
     def integrity_validation(self, obj, real_obj=None):
