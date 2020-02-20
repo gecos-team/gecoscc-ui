5e9437f726fb3117623a15ae9436ec0d6c04c3a6
System25 <amp_21004@yahoo.es>
2020-02-20 08:26:20 +0100

    Optimize dict_merge operation

--- gecoscc/utils.py
+++ gecoscc/utils.py
@@ -410,16 +410,18 @@ def to_deep_dict(node_attr):
 
 def dict_merge(a, b):
     '''recursively merges dict's. not just simple a['key'] = b['key'], if
-    both a and bhave a key who's value is a dict then dict_merge is called
+    both a and b have a key who's value is a dict then dict_merge is called
     on both values and the result stored in the returned dictionary.'''
     if not isinstance(b, dict):
         return b
-    result = deepcopy(a)
+    result = a.copy()
     for k, v in b.iteritems():
         if k in result and isinstance(result[k], dict):
                 result[k] = dict_merge(result[k], v)
+        elif isinstance(v, list):
+            result[k] = list(v)
         else:
-            result[k] = deepcopy(v)
+            result[k] = v
     return result
 
 
